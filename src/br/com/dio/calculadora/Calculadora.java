fccdf2eb9cc5cea10e360de0c293bcadb9733b18
Eduarda Emilli <emillieduarda28@gmail.com>
2021-08-15 18:48:12 -0300

    first commit

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,40 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main (String[] args) }
+		Scanner scan = new Scanner (System.in); {
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: " );
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: " );
+		b = scan.nextInt();
+		
+		int soma = (a,b);
+		int subtracao(a,b);
+	}
+
+	
+	public static int soma (int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtracao (int a, int b) {
+		return a - b;
+	}
+	
+	public static int divisao (int a, int b) {
+		return a / b;
+	}
+	
+	public static int multiplicacao (int a, int b) {
+		return a * b;
+	}
+	
+	
+	
+}
